dc1a0c760141bd5ace711e799667822aecc9fbc8
Alex Huryn <algurin77@gmail.com>
2024-10-31 11:55:22 +0100

    set up express + prisma

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-import path from 'path';
-require('dotenv').config({
-    override: true,
-    path: path.join(__dirname, 'development.env'),
-});
-import pg from 'pg';
+import express from 'express';
+const app = express();
 
-const pool = new pool({
-    DB_USER: process.env.DB_USER,
-    DB_HOST: process.env.DB_HOST,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_PORT: process.env.DB_PORT,
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
 });
+
+app.get('/test', (req, res) => {
+    res.send('Test route is working!');
+});
+
+export default app;
--- index.js
+++ index.js
@@ -1,54 +1,9 @@
-// server file
-
+// server.js
 import http from "http";
-import fs from "fs";
-import path from "path";
+import app from "./app.js";
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const server = http.createServer(app);
 
-const server = http.createServer((req, res) => {
-    let filePath = req.url === '/' ? 'app.js' : req.url.substring(1);
-
-    const extname = path.extname(filePath);
-    let contentType = 'text/html';
-
-    switch (extname) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'application/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.ico':
-            contentType = 'image/x-icon';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-    fs.readFile(filePath, (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/plain');
-            res.end('404 Not Found\n');
-            return;
-        }
-        res.statusCode = 200;
-        res.setHeader('Content-Type', contentType);
-        res.end(data);
-    });
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(4000, () => {
+    console.log('server running at http://localhost:4000');
 });
